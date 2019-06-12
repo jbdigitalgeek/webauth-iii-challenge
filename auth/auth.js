599137f73ecce074bcf17bb4588611bafb011073
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-06-12 12:31:01 -0700

    router in working on tables

---
+++ auth/auth.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcryptjs');
+const Users = require('../api/user-model');
+
+
+function protected(req, res, next) {
+  const { username, password } = req.headers;
+   
+
+  if (req.session && req.session.username) { 
+    next();
+  } else {
+    if (username && password) {
+      Users
+        .findBy({ username })
+        .first()
+        .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+            next();
+          } else {
+            res.status(401).json({ message: 'Unauthorized' });
+          }
+        })
+        .catch(error => {
+          res.status(500).json({ error: `${error}` });
+        });
+    } else {
+      res.status(401).json({ message: 'Unauthorized' });
+    }
+  }
+}
+
+ module.exports = protected;
---
+++ data/routers/router.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const Users = require('./user-model');
+const protected = require('../auth/auth-middleware');
+const bcrypt = require('bcryptjs');
+
+
+router.post('/register', (req, res) => {
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 12);
+
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json({ error: `${error}` });
+    });
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.username = user.username;
+        res.status(200).json({ message: `Hello ${user.username}!` });
+      } else {
+        res.status(401).json({ message: ' Unauthorized ' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: `${error}` });
+    });
+});
+
+ router.get('/users', protected, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => res.send(error));
+});
+
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(error => {
+      if (error) {
+        res.send('Unable to logout');
+      } else {
+        res.send('Logged out');
+      }
+    });
+  } else {
+    res.end();
+  }
+});
+
+module.exports = router;
