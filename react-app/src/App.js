ff9bcb088b07e2878ff58e309dbf9f33fe68233e
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-06-13 13:32:53 -0700

    building out dom router/axios calls

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Route, NavLink } from 'react-router-dom';
 import './App.css';
-import Login from './login/Login'
+import Login from './login/Login';
+import UserList from './users/UserList';
 
-function App() {
-  return (
-    <>
-      <header>
-        <NavLink to="/login">Login</NavLink>
-      </header>
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <header>
+          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/users">Users</NavLink>
+          <button onClick={this.logout}>Logout</button>
+        </header>
         <main>
+        <Route path="/users" component={UserList}></Route>
           <Route path="/login" component={Login}></Route>
-   </main>
+        </main>
       
-    </>
-  );
-}
+      </>
+    );
+  }
+};
 
 export default App;
--- react-app/src/login/Login.js
+++ react-app/src/login/Login.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+
 class Login extends React.Component {
     render() {
-        return <h3>Login Component</h3>
+       return <>
+         <h3>Login Component</h3>
+          <form action="">
+              <div><label htmlFor="username"></label><input type="text"/></div>
+              <div><label htmlFor="password"></label><input type="text"/></div>
+              <div><button></button></div>
+          </form>
+        </>;
+        }
     }
-}
 export default Login;
